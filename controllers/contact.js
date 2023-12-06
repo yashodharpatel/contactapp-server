83dee6c4cfdd5fb041bd287b7c662abaaad5e844
yashodharpatel <patelyashodhar012@gmail.com>
2023-12-06 21:38:51 +0530

    formatting done

--- controllers/contact.js
+++ controllers/contact.js
@@ -1,29 +1,41 @@
-async function getContacts(req, res) {
+// @desc Get ALL Contacts
+// @route GET /contact/get-contacts
+// @access PUBLIC
+const getContacts = async (req, res) => {
   try {
     res.status(200).json({ message: "Get All contacts" });
   } catch (error) {
     console.log(error);
   }
-}
+};
 
-async function getContact(req, res) {
+// @desc Get single contact
+// @route GET /contact/get-contact/:id
+// @access PUBLIC
+const getContact = async (req, res) => {
   try {
     res.status(200).json({ message: `Get Contact with id ${req.params.id}` });
   } catch (error) {
     console.log(error);
   }
-}
+};
 
-async function addContact(req, res) {
+// @desc Add new contact
+// @route POST /contact/add-contact
+// @access PUBLIC
+const addContact = async (req, res) => {
   try {
-    console.log(req.body)
+    console.log(req.body);
     res.status(201).json({ message: "Added new contact" });
   } catch (error) {
     console.log(error);
   }
-}
+};
 
-async function updateContact(req, res) {
+// @desc Update Contact
+// @route PUT /contact/update-contact/:id
+// @access PUBLIC
+const updateContact = async (req, res) => {
   try {
     res
       .status(200)
@@ -31,9 +43,12 @@ async function updateContact(req, res) {
   } catch (error) {
     console.log(error);
   }
-}
+};
 
-async function deleteContact(req, res) {
+// @desc Delete Contact
+// @route DELETE /contact/delete-contact/:id
+// @access PUBLIC
+const deleteContact = async (req, res) => {
   try {
     res
       .status(200)
@@ -41,7 +56,7 @@ async function deleteContact(req, res) {
   } catch (error) {
     console.log(error);
   }
-}
+};
 
 export default {
   getContacts,
