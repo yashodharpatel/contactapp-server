3ef969ca7b437c8f1e112abdfd5c49fe0c6715d6
yashodharpatel <patelyashodhar012@gmail.com>
2023-12-10 12:14:20 +0530

    errhandler added

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 import express from "express";
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
+import morgan from "morgan";
 import contactRouter from "#routes/contact";
 import indexRouter from "#routes/index";
+import errorHandler from "#middleware/errorHandler";
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 8000;
 
 app.use(express.json());
+app.use(morgan('dev'));
 
 app.use("/", indexRouter);
 app.use("/contact", contactRouter);
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log("Contact App Server successfully running on PORT", PORT);
--- controllers/contact.js
+++ controllers/contact.js
@@ -1,62 +1,44 @@
+import asyncHandler from "express-async-handler";
+
 // @desc Get ALL Contacts
 // @route GET /contact/get-contacts
 // @access PUBLIC
-const getContacts = async (req, res) => {
-  try {
-    res.status(200).json({ message: "Get All contacts" });
-  } catch (error) {
-    console.log(error);
-  }
-};
+const getContacts = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "Get All contacts" });
+});
 
 // @desc Get single contact
 // @route GET /contact/get-contact/:id
 // @access PUBLIC
-const getContact = async (req, res) => {
-  try {
-    res.status(200).json({ message: `Get Contact with id ${req.params.id}` });
-  } catch (error) {
-    console.log(error);
-  }
-};
+const getContact = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: `Get Contact with id ${req.params.id}` });
+});
 
 // @desc Add new contact
 // @route POST /contact/add-contact
 // @access PUBLIC
-const addContact = async (req, res) => {
-  try {
-    console.log(req.body);
-    res.status(201).json({ message: "Added new contact" });
-  } catch (error) {
-    console.log(error);
+const addContact = asyncHandler(async (req, res) => {
+  const { name, email } = req.body;
+  if (!name || !email) {
+    res.status(400);
+    throw new Error("All fields are mandotary");
   }
-};
+  res.status(201).json({ message: "Added new contact" });
+});
 
 // @desc Update Contact
 // @route PUT /contact/update-contact/:id
 // @access PUBLIC
-const updateContact = async (req, res) => {
-  try {
-    res
-      .status(200)
-      .json({ message: `Updated Contact with id ${req.params.id}` });
-  } catch (error) {
-    console.log(error);
-  }
-};
+const updateContact = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: `Updated Contact with id ${req.params.id}` });
+});
 
 // @desc Delete Contact
 // @route DELETE /contact/delete-contact/:id
 // @access PUBLIC
-const deleteContact = async (req, res) => {
-  try {
-    res
-      .status(200)
-      .json({ message: `Deleted Contact with id ${req.params.id}` });
-  } catch (error) {
-    console.log(error);
-  }
-};
+const deleteContact = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: `Deleted Contact with id ${req.params.id}` });
+});
 
 export default {
   getContacts,
